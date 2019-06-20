0b48ca171d34f89772b07b5ded7565541bda7efc
Gui <33119610+GuiMarion@users.noreply.github.com>
2019-06-20 11:36:04 +0200

    ok

--- figs/server/resultsEEG/plot.py
+++ figs/server/resultsEEG/plot.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import scipy.io as sio
 
 sys.path.append("../../../")
 from idyom import data
@@ -9,7 +10,7 @@ sys.path.append("stimuli/giovanni/surprises/")
 
 SERVER = False
 
-folder = "../../../figs/server/stimuli/"
+folder = "../../../figs/server/EEG/"
 
 def compareLikelihoods(x1, x2, name="kikou.eps"):
 
@@ -34,13 +35,24 @@ def compareLikelihoods(x1, x2, name="kikou.eps"):
 	plt.savefig(name+"_2.eps")
 	plt.show()
 
-file1 = "surpriseSignal_lisp.pickle"
-file2 = "surpriseSignal_bach_Pearce_jump_False_relativeEntropy.pickle"
-file3 = "surpriseSignal_bach_Pearce_jump_True.pickle"
+def load(file):
+	dat = sio.loadmat(file)['data'][0]
+	names = ['audio1', 'audio2', 'audio3', 'audio4', 'audio5', 'audio6', 'audio7', 'audio8', 'audio9', 'audio10']
+	res = {}
+	for i in range(len(dat)):
+		res[names[i]] = dat[i]
 
-l1 = pickle.load( open(file1, "rb" ) )
-l2 = pickle.load( open(file2, "rb" ) )
-l3 = pickle.load( open(file3, "rb" ) )
+	return res
+file1 = "Lisp.mat"
+file2 = "Python.mat"
+file3 = "Jump.mat"
+
+l1 = load(file1)
+l2 = load(file2)
+l3 = load(file3)
+
+for key in l1:
+	print(l1[key], l2[key], l3[key])
 
 likelihoods1 = []
 likelihoods2 = []
@@ -49,12 +61,9 @@ likelihoods3 = []
 
 for file in l1:
 
-	l3[file] = np.nan_to_num(l3[file])
-	l1[file] = np.nan_to_num(l1[file])
-
 	likelihoods1.append(np.mean(l1[file]))
-	likelihoods2.append(2**-np.mean(l2[file]))
-	likelihoods3.append(2**-np.mean(l3[file]))
+	likelihoods2.append(np.mean(l2[file]))
+	likelihoods3.append(np.mean(l3[file]))
 
 plt.bar([1,2,3], [np.mean(likelihoods1), np.mean(likelihoods2), np.mean(likelihoods3)], yerr=[np.std(likelihoods1), np.std(likelihoods2), np.std(likelihoods3)])
 plt.savefig(folder+"comparisonsIDYOM_IDYOMpy_JUMP.eps")
@@ -65,7 +74,7 @@ compareLikelihoods(likelihoods2, likelihoods1, name=folder+"compareLikelihoodsID
 # ploting in the music space
 
 M = data.data()
-M.parse("../", augment=False)
+M.parse("../../../stimuli/giovanni/", augment=False)
 
 dat2, files4 = M.getScoresFeatures()
 
@@ -73,7 +82,7 @@ weights = []
 colors = []
 
 for file in range(len(likelihoods1)):
-	weights.append(80000*abs(likelihoods1[file]-likelihoods2[file])**2)
+	weights.append(80000000*abs(likelihoods1[file]-likelihoods2[file])**2)
 	if likelihoods1[file]-likelihoods2[file] < 0:
 		colors.append('coral')
 	elif likelihoods1[file]-likelihoods2[file] > 0:
@@ -103,16 +112,13 @@ compareLikelihoods(likelihoods2, likelihoods3, name=folder+"compareLikelihoodsID
 
 # ploting in the music space
 
-M = data.data()
-M.parse("../", augment=False)
-
 dat2, files4 = M.getScoresFeatures()
 
 weights = []
 colors = []
 
 for file in range(len(likelihoods3)):
-	weights.append(80000*abs(likelihoods3[file]-likelihoods2[file])**2)
+	weights.append(80000000*abs(likelihoods3[file]-likelihoods2[file])**2)
 	if likelihoods3[file]-likelihoods2[file] < 0:
 		colors.append('coral')
 	elif likelihoods3[file]-likelihoods2[file] > 0:
